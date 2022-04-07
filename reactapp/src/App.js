f4851ea0eda1b2b6c9a3138e58ec2be512bd9f4d
Subhodip Chatterjee <73605031+Subhodip11@users.noreply.github.com>
2022-04-07 22:41:57 +0530

    Update App.js

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import Admin from './Components/Admin/Admin';
+import AdminRegistration from './Components/Registration/AdminRegistration';
+import UserRegistration from './Components/Registration/UserRegistration';
 import SuperAdmin from './Components/SuperAdmin/SuperAdmin';
 import User from "./Components/User/User";
 
@@ -8,10 +10,11 @@ import User from "./Components/User/User";
 function App() {
   return (
     <>
-
+    <UserRegistration/>
+    {/* <AdminRegistration/> */}
     {/* <User/> */}
     {/* <Admin/> */}
-    <SuperAdmin/>
+    {/* <SuperAdmin/> */}
     </>
   );
 }
